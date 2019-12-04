8c99af4368bce4965e71accb8e9ff6cdbc6b78aa
“Gromy1211git config --global user.email ”Gromy1211@outlook.com <33060023+Gromy1211@users.noreply.github.com>
2019-12-04 16:49:12 +0800

    Update login demo

--- run.py
+++ run.py
@@ -44,7 +44,7 @@ def is_login():
 
 #根据userID在数据库中查询username
 def fillinusername(userID):
-    sql="select username from dbo.[user] where userID='"+userID+"'"
+    sql="select username from dbo.[user] where email='"+userID+"'"
     cursor.execute(sql)
     userName=cursor.fetchall()
     try:
@@ -68,22 +68,21 @@ def logout():
 def login():
     if request.method=='GET':
         #userID和password匹配后，根据不同的role重定向至不同页面
+
         if session.get('userID'):
             if session.get('role')== 'GM' :
-                return redirect(url_for('GM'))
+                return redirect(url_for('indexGM'))
             elif session.get('role')== 'AM' :
-                return redirect(url_for('AM'))
+                return redirect(url_for('indexAM'))
             elif session.get('role')=='PM':
-                return redirect(url_for('PM'))
+                return redirect(url_for('indexPM'))
         else:
             return render_template('index.html', error=None)
-    else:
+    elif request.method=='POST':
         error=None
-
-#============================================================================
-#需要等待前端表单设置
-        userID = request.form['username']
-        pwd = request.form['passwd']
+        userID = request.form['loginemail']
+        pwd = request.form['loginpassword']
+        
 #============================================================================    
 # 用户名 密码是否都已经输入    
         if not all([userID,pwd]):
@@ -95,18 +94,18 @@ def login():
                 return render_template('index.html',error=error)
 # 在数据库中查询用户名 密码是否匹配
         arg = (userID,pwd)
-        sql1 = "select userID from dbo.[user] where userID=%s and password=%s"
+        sql1 = "select email from dbo.[user] where email=%s and password=%s"
         cursor.execute(sql1,arg)
         rs_userid = cursor.fetchall()
- #在数据库中根据用户名查询身份
-        sql2 = "select role from dbo.user where userID ='"+userID+"'"
-
+                
 # 用户名和密码匹配        
         if(len(rs_userid) != 0):
             #用户登录设置session的userID和username
             session['userID']=userID
             session['username']=fillinusername(userID)
 
+            #在数据库中根据用户名查询身份
+            sql2 = "select role from [user] where email =\'"+userID+"\'"
             cursor.execute(sql2)
             rs_roleid= cursor.fetchall()
             roleID=sorted(rs_roleid)[-1][0] # 取最大的roleID，避免多重身份影响
@@ -114,16 +113,31 @@ def login():
             if(roleID==1):
                 #将用户角色加入session
                 session['role']='GM'
-                return redirect(url_for('GM'))
+                return redirect(url_for('indexGM'))
             elif(roleID==2):
                 session['role']='AM'
-                return redirect(url_for('AM'))
+                return redirect(url_for('indexAM'))
             elif(roleID==3):
                 session['role']='PM'
-                return redirect(url_for('PM'))
+                return redirect(url_for('indexPM'))
         else: # 用户名和密码不匹配    
             error="账号或密码错误"
             return render_template('index.html',error = error)
 
 def deny():
     return "Permission denied"
+
+@app.route('/AM/indexAM', methods=['GET','POST'])
+def indexAM():
+    return render_template('indexAM.html')
+
+@app.route('/GM/indexGM', methods=['GET','POST'])
+def indexGM():
+    return render_template('indexGM.html')
+
+@app.route('/PM/indexPM', methods=['GET','POST'])
+def indexPM():
+    return render_template('indexPM.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
